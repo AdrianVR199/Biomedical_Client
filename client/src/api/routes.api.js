8370890faf70a102e14f1c12c3f763385bee8f3f
Adrian Vargas <adrian.vargas@uao.edu.co>
2023-06-27 14:37:25 -0500

    cambio client 2

--- client/src/api/routes.api.js
+++ client/src/api/routes.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //const serverName="localhost:8080";
-const serverName="ec2-3-83-2-193.compute-1.amazonaws.com";
+const serverName="ec2-3-83-2-193.compute-1.amazonaws.com:443";
 
 const port="4000";
 //logearse en la aplicacion
@@ -9,26 +9,26 @@ export const loginUserRequest = async (user) =>
     method: "POST",
     data: user,
     withCredentials: true,
-    url: `http://${serverName}/auth/signin`,
-// url: "http://localhost:4000/auth/signin",
+    url: `https://${serverName}/auth/signin`,
+// url: "https://localhost:4000/auth/signin",
   });
 
 export const loadUserRequest = async () =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/userinfo`,
+    url: `https://${serverName}/userinfo`,
   });
 
 export const loadFullUserRequest = async (id) =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/userinfoC/${id}`,
+    url: `https://${serverName}/userinfoC/${id}`,
   });
 
 export const loginUserRequest1 = async (user) =>
-  await axios.post(`http://${serverName}/auth/signin`, user);
+  await axios.post(`https://${serverName}/auth/signin`, user);
 
 //Registrarse en la aplicacion
 export const signupUserRequest = async (user) =>
@@ -36,14 +36,14 @@ export const signupUserRequest = async (user) =>
     method: "POST",
     data: user,
     withCredentials: true,
-    url: `http://${serverName}/auth/signup`,
+    url: `https://${serverName}/auth/signup`,
   });
 //Cerrar sesion
 export const CerrarSesionRequest = async () =>
   await axios({
     method: "POST",
     withCredentials: true,
-    url: `http://${serverName}/logout`,
+    url: `https://${serverName}/logout`,
   });
 
 //actualizar datos usuario
@@ -52,10 +52,10 @@ export const ActualizarUsuarioRequest = async (id, newfields) =>
     method: "PUT",
     withCredentials: true,
     data: newfields,
-    url: `http://${serverName}/usuarios/${id}`,
+    url: `https://${serverName}/usuarios/${id}`,
   });
 
-// await axios.post("http://${serverName}/logout");
+// await axios.post("https://${serverName}/logout");
 
 //CITAS
 
@@ -65,32 +65,32 @@ export const createCitaRequest = async (cita) =>
     method: "POST",
     data: cita,
     withCredentials: true,
-    url: `http://${serverName}/citas`,
+    url: `https://${serverName}/citas`,
   });
-//await axios.post("http://${serverName}/tasks/", cita);
+//await axios.post("https://${serverName}/tasks/", cita);
 
 //traer cita
 export const getCitaRequest = async (id) =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/citas/${id}`,
+    url: `https://${serverName}/citas/${id}`,
   });
-// await axios.get(`http://${serverName}/tasks/${id}`);
+// await axios.get(`https://${serverName}/tasks/${id}`);
 
 //Crear cita
 export const getCitasRequest = async () =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/citas`,
+    url: `https://${serverName}/citas`,
   });
-// await axios.get("http://${serverName}/citas", { withCredentials: true });
+// await axios.get("https://${serverName}/citas", { withCredentials: true });
 export const getCitasCRequest = async () =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/citascompletas`,
+    url: `https://${serverName}/citascompletas`,
   });
 //actualizar cita
 export const UpdateCitaRequest = async (id, newfields) =>
@@ -98,18 +98,18 @@ await axios({
   method: "PUT",
   withCredentials: true,
   data: newfields,
-  url: `http://${serverName}/citas/${id}`,
+  url: `https://${serverName}/citas/${id}`,
 });
- // await axios.put(`http://${serverName}/tasks/${id}`, newfields);
+ // await axios.put(`https://${serverName}/tasks/${id}`, newfields);
 
 //borrar cita
 export const DeleteCitaRequest = async (id) =>
   await axios({
     method: "DELETE",
     withCredentials: true,
-    url: `http://${serverName}/citas/${id}`,
+    url: `https://${serverName}/citas/${id}`,
   });
-// await axios.delete(`http://${serverName}/tasks/${id}`);
+// await axios.delete(`https://${serverName}/tasks/${id}`);
 
 //traer horarios
 export const getHorariosRequest = async (infoh) =>
@@ -117,27 +117,27 @@ export const getHorariosRequest = async (infoh) =>
     method: "POST",
     withCredentials: true,
     data: infoh,
-    url: `http://${serverName}/horarios`,
+    url: `https://${serverName}/horarios`,
   });
 
 //HISTORIALES
 
 //Crear historial
 export const createHistorialRequest = async (hist) =>
-  await axios.post(`http://${serverName}/tasks/`, hist);
+  await axios.post(`https://${serverName}/tasks/`, hist);
 
 //traer historial
 export const getHistorialRequest = async (id) =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/historiales/${id}`,
+    url: `https://${serverName}/historiales/${id}`,
   });
-// await axios.get(`http://${serverName}//historiales/:${id}`);
+// await axios.get(`https://${serverName}//historiales/:${id}`);
 
 //traer historiales
 export const getHistorialesRequest = async () =>
-  await axios.post(`http://${serverName}/tasks/`);
+  await axios.post(`https://${serverName}/tasks/`);
 
 //actualizar historial
 export const UpdateHistorialRequest = async (id, newfields) =>
@@ -145,36 +145,36 @@ await axios({
   method: "PUT",
   withCredentials: true,
   data: newfields,
-  url: `http://${serverName}/historiales/${id}`,
+  url: `https://${serverName}/historiales/${id}`,
 });
- // await axios.put(`http://${serverName}/tasks/${id}`, newfields);
+ // await axios.put(`https://${serverName}/tasks/${id}`, newfields);
 
 //eliminar hisotrial
 export const DeleteHistorialRequest = async (id) =>
-  await axios.delete(`http://${serverName}/tasks/${id}`);
+  await axios.delete(`https://${serverName}/tasks/${id}`);
 
 //usuarioooooos
 
 //traer historial
 export const getUsuarioRequest = async (id) =>
-  await axios.get(`http://${serverName}/usuarios/${id}`);
+  await axios.get(`https://${serverName}/usuarios/${id}`);
 
 //traer historiales
 export const getUsuariosRequest = async () =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/usuarios`,
+    url: `https://${serverName}/usuarios`,
   });
-// await axios.post("http://${serverName}/usuarios/");
+// await axios.post("https://${serverName}/usuarios/");
 
 //actualizar historial
 export const UpdateUsuarioRequest = async (id, newfields) =>
-  await axios.put(`http://${serverName}/usuarios/${id}`, newfields);
+  await axios.put(`https://${serverName}/usuarios/${id}`, newfields);
 
 //eliminar hisotrial
 export const DeleteUsuarioRequest = async (id) =>
-  await axios.delete(`http://${serverName}/usuarios/${id}`);
+  await axios.delete(`https://${serverName}/usuarios/${id}`);
 
 //data de depa y pueblos
 
@@ -182,14 +182,14 @@ export const loadDepartamentosRequest = async () =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/departamentos`,
+    url: `https://${serverName}/departamentos`,
   });
 
 export const loadCiudadesRequest = async (id) =>
   await axios({
     method: "GET",
     withCredentials: true,
-    url: `http://${serverName}/ciudades/${id}`,
+    url: `https://${serverName}/ciudades/${id}`,
   });
 
 export const loadCorreosRequest = async (data) =>
@@ -197,5 +197,5 @@ export const loadCorreosRequest = async (data) =>
     method: "POST",
     data:data,
     withCredentials: true,
-    url: `http://${serverName}/correos`,
+    url: `https://${serverName}/correos`,
   });
